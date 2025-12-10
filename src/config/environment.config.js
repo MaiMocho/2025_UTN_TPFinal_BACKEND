2655063ac696b577f199ea3429d0f6f5a5783daa
Maia.dev <asmodeoanxy@gmail.com>
2025-12-09 23:22:37 -0300

    xfavor que esto funcione

--- src/config/environment.config.js
+++ src/config/environment.config.js
@@ -1,11 +1,7 @@
 import dotenv from 'dotenv'
 
-
-//Cargar las variables de entorno en process.env
 dotenv.config()
 
-
-//Diccionario donde guando mis variables de entorno
 const ENVIRONMENT = {
     GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
     GMAIL_USER: process.env.GMAIL_USER,
@@ -19,8 +15,6 @@ const ENVIRONMENT = {
     MYSQL_DB_HOSTNAME: process.env.MYSQL_DB_HOSTNAME,
     MYSQL_DB_USERNAME: process.env.MYSQL_DB_USERNAME, 
     MYSQL_DB_PASSWORD: process.env.MYSQL_DB_PASSWORD,
-    /*   MONGO_DB_HOST: process.env.MONGO_DB_HOST,
-      MONGO_DB_NAME: process.env.MONGO_DB_NAME, */
 }
 
 
--- src/config/mailTransporter.config.js
+++ src/config/mailTransporter.config.js
@@ -3,14 +3,15 @@ import ENVIRONMENT from './environment.config.js'
 
 const mailTransporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465, 
-    secure: true,
+    port: 587,            
+    secure: false,   
     auth: {
         user: ENVIRONMENT.GMAIL_USER,
         pass: ENVIRONMENT.GMAIL_PASSWORD
     },
-    family: 4,
-    connectionTimeout: 10000
+    family: 4,             
+    connectionTimeout: 10000, 
+    greetingTimeout: 5000
 })
 
 export default mailTransporter
