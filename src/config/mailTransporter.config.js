e3dce50888687f00741d3a97f1ab2dedff46fbe9
Maia.dev <asmodeoanxy@gmail.com>
2025-12-09 23:11:55 -0300

    help

--- src/config/mailTransporter.config.js
+++ src/config/mailTransporter.config.js
@@ -3,15 +3,14 @@ import ENVIRONMENT from './environment.config.js'
 
 const mailTransporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, 
+    port: 465, 
+    secure: true,
     auth: {
         user: ENVIRONMENT.GMAIL_USER,
         pass: ENVIRONMENT.GMAIL_PASSWORD
     },
-    tls: {
-        rejectUnauthorized: false
-    }
+    family: 4,
+    connectionTimeout: 10000
 })
 
 export default mailTransporter
