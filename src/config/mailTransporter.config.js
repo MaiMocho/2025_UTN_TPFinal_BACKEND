8a7e626cf73496236d1258cc236871d2c4c2f80c
Maia.dev <asmodeoanxy@gmail.com>
2025-12-09 23:25:03 -0300

    oa

--- src/config/mailTransporter.config.js
+++ src/config/mailTransporter.config.js
@@ -7,7 +7,7 @@ const mailTransporter = nodemailer.createTransport({
     secure: false,   
     auth: {
         user: ENVIRONMENT.GMAIL_USER,
-        pass: ENVIRONMENT.GMAIL_PASSWORD
+        pass: ENVIRONMENT.GMAIL_PASSWORD //hola
     },
     family: 4,             
     connectionTimeout: 10000, 
