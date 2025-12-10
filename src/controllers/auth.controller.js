361cdc1498bf68d11c3e7eb5af0f9acd74bef8e8
Maia.dev <asmodeoanxy@gmail.com>
2025-12-10 00:01:16 -0300

    Volvemos una para atrás

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,37 +3,38 @@ import { ServerError } from "../error.js";
 import AuthService from "../services/auth.service.js";
 
 class AuthController {
-    async register(req, res) {
-    try {
-        const { username, email, password } = req.body;
-        
-        const existingUser = await UserRepository.findUserByEmail(email);
-        if (existingUser) {
-            return res.status(400).json({ message: 'El email ya está en uso' });
+    static async register (request, response){
+        try{
+            console.log('Registro recibido:', request.body)
+            const { email, password, name } = request.body
+            
+            await AuthService.register(email, password, name)
+            response.status(201).json({
+                ok: true,
+                message: 'Usuario registrado con exito',
+                status: 201
+            })
         }
-
-        const newUser = await AuthService.register({ username, email, password }); 
-
-        try {
-            await sendEmail({
-                to: email,
-                subject: 'Valida tu cuenta',
-                html: `<h1>Bienvenido ${username}!</h1><a href="${ENVIRONMENT.URL_FRONTEND}/verify/${newUser.verificationToken}">Verificar cuenta</a>`
-            });
-        } catch (mailError) {
-            console.error("Error al enviar el email:", mailError.message);
+        catch(error){
+            if(error.status){
+                response.status(error.status).json({
+                    ok:false,
+                    message: error.message,
+                    status: error.status
+                })
+            }
+            else{
+                console.error(
+                    'ERROR AL REGISTRAR', error
+                )
+                response.status(500).json({
+                    ok: false,
+                    message: 'Error interno del servidor',
+                    status: 500
+                })
+            }
         }
-
-        return res.status(201).json({ 
-            message: 'Usuario registrado con éxito',
-            user: newUser
-        });
-
-    } catch (error) {
-        console.error("Error fatal:", error);
-        res.status(500).json({ message: 'Error interno del servidor', error: error.message });
     }
-}
 
     static async verifyEmail (request, response){
         try{
