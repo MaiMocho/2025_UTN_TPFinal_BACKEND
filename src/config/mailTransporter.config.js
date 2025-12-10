2c8ba8fd7e36ffe7fd203fb9488e9c657ea6af8a
Maia.dev <asmodeoanxy@gmail.com>
2025-12-09 22:51:14 -0300

    Fix del nodemailer

--- src/config/mailTransporter.config.js
+++ src/config/mailTransporter.config.js
@@ -2,13 +2,15 @@ import nodemailer from 'nodemailer'
 import ENVIRONMENT from './environment.config.js'
 
 const mailTransporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, 
     auth: {
         user: ENVIRONMENT.GMAIL_USER,
         pass: ENVIRONMENT.GMAIL_PASSWORD
     },
     tls: {
-        rejectUnauthorized: false //Ignoramos validaciones de certificado TLS
+        rejectUnauthorized: false
     }
 })
 
