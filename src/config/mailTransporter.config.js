a14469f6c58cdf5bfb273eedd96e9ebcedb131ec
Maia.dev <asmodeoanxy@gmail.com>
2025-12-16 14:04:48 -0300

    debug log del nodemailer

--- src/config/mailTransporter.config.js
+++ src/config/mailTransporter.config.js
@@ -2,14 +2,19 @@ import nodemailer from 'nodemailer'
 import ENVIRONMENT from './environment.config.js'
 
 const mailTransporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: ENVIRONMENT.GMAIL_USER,
         pass: ENVIRONMENT.GMAIL_PASSWORD
     },
     tls: {
         rejectUnauthorized: false
-    }
+    },
+    family: 4,  
+    debug: true,  
+    logger: true  
 })
 
 export default mailTransporter
