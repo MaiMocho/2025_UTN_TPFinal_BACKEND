c148729c8a5ce05db0fa5f08db7ce9c15e01c781
Maia.dev <asmodeoanxy@gmail.com>
2025-12-16 12:49:55 -0300

    Probando Brevo

--- src/config/mailTransporter.config.js
+++ src/config/mailTransporter.config.js
@@ -2,12 +2,17 @@ import nodemailer from 'nodemailer'
 import ENVIRONMENT from './environment.config.js'
 
 const mailTransporter = nodemailer.createTransport({
-    service: 'hotmail',
+    host: 'smtp-relay.brevo.com',
+    port: 2525,
+    secure: false, 
     auth: {
         user: ENVIRONMENT.GMAIL_USER,
         pass: ENVIRONMENT.GMAIL_PASSWORD
     },
-    family: 4 
+    tls: {
+        rejectUnauthorized: false
+    },
+    family: 4
 })
 
 export default mailTransporter
